e63f04de8d34c21a882d9eb1e5a9d130ef36a841
lihaibo42 <lihaibo42@jd.com>
2021-09-09 14:42:23 +0800

    [BugFix]Add sessions/session_id/xid to snapshot file

--- src/Service/NuRaftStateMachine.cpp
+++ src/Service/NuRaftStateMachine.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <mutex>
 #include <string>
+#include <math.h>
 #include <Service/NuRaftStateMachine.h>
 #include <Service/ReadBufferFromNuraftBuffer.h>
 #include <Service/WriteBufferFromNuraftBuffer.h>
@@ -205,8 +206,17 @@ NuRaftStateMachine::NuRaftStateMachine(
             }
             for (auto request : *(batch.request_vec))
             {
-                //replay(batch_start_index, entry);
                 storage.processRequest(request->request, request->session_id);
+                if (request->session_id > storage.session_id_counter)
+                {
+                    LOG_WARNING(log, "Storage's session_id_counter {} must more than the session id {} of log.", storage.session_id_counter, request->session_id);
+                    storage.session_id_counter = request->session_id;
+                }
+                if (request->request->xid > storage.zxid.load(std::memory_order_acquire))
+                {
+                    //LOG_WARNING(log, "Storage's zxid {} must more than session id {} of log.", storage.xid, request->request->xid);
+                    storage.zxid.store(request->request->xid, std::memory_order_release);
+                }
             }
             log_queue.pop();
             last_committed_idx = batch.batch_end_index - 1;
