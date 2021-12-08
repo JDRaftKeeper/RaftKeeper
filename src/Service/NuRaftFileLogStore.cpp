009eac7253d403d033d878b20e9c2f52a6a28e8a
zhangxiao871 <zhangxiao871@jd.com>
2021-12-08 11:06:21 +0800

    [JD_RAFT] add log to entry_at

--- src/Service/NuRaftFileLogStore.cpp
+++ src/Service/NuRaftFileLogStore.cpp
@@ -221,7 +221,7 @@ ptr<log_entry> NuRaftFileLogStore::entry_at(ulong index)
         if (src == nullptr)
         {
             src = segment_store->getEntry(index);
-            LOG_DEBUG(log, "get entry {} from disk", index);
+            LOG_TRACE(log, "get entry {} from disk", index);
             //2^16, 65536
             if (index << 48 == 0)
             {
@@ -230,7 +230,7 @@ ptr<log_entry> NuRaftFileLogStore::entry_at(ulong index)
         }
         else
         {
-            LOG_DEBUG(log, "get entry {} from queue", index);
+            LOG_TRACE(log, "get entry {} from queue", index);
         }
     }
     if (src)
