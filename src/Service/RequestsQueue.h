aadc15f50260ef5f903ad6c6c6b2732bad2b4401
zhangxiao871 <zhangxiao871@jd.com>
2022-07-15 16:28:35 +0800

    request queue use BlockingConcurrentQueue

--- src/Service/RequestsQueue.h
+++ src/Service/RequestsQueue.h
@@ -2,13 +2,21 @@
 
 #include <Common/ConcurrentBoundedQueue.h>
 #include <Service/NuRaftStateMachine.h>
+#include <boost/lockfree/queue.hpp>
+#include <blockingconcurrentqueue.h>
 
 namespace DB
 {
+using namespace moodycamel;
 
 struct RequestsQueue
 {
-    using Queue = ConcurrentBoundedQueue<SvsKeeperStorage::RequestForSession>;
+//    using Queue = ConcurrentBoundedQueue<SvsKeeperStorage::RequestForSession>;
+
+//    using Queue = boost::lockfree::queue<SvsKeeperStorage::RequestForSession>;
+
+    using Queue = BlockingConcurrentQueue<SvsKeeperStorage::RequestForSession>;
+
     std::vector<ptr<Queue>> queues;
 
     explicit RequestsQueue(size_t child_queue_size, size_t capacity = 20000)
@@ -25,31 +33,37 @@ struct RequestsQueue
 
     bool push(const SvsKeeperStorage::RequestForSession & request)
     {
-        return queues[request.session_id % queues.size()]->push(std::forward<const SvsKeeperStorage::RequestForSession>(request));
+        //return queues[request.session_id % queues.size()]->push(std::forward<const SvsKeeperStorage::RequestForSession>(request));
+        return queues[request.session_id % queues.size()]->enqueue(std::forward<const SvsKeeperStorage::RequestForSession>(request));
     }
 
-    bool tryPush(const SvsKeeperStorage::RequestForSession & request, UInt64 wait_ms = 0)
+    bool tryPush(const SvsKeeperStorage::RequestForSession & request, UInt64 /*wait_ms = 0*/)
     {
-        return queues[request.session_id % queues.size()]->tryPush(
-            std::forward<const SvsKeeperStorage::RequestForSession>(request), wait_ms);
+//        return queues[request.session_id % queues.size()]->tryPush(
+//            std::forward<const SvsKeeperStorage::RequestForSession>(request), wait_ms);
+        return queues[request.session_id % queues.size()]->try_enqueue(
+            std::forward<const SvsKeeperStorage::RequestForSession>(request));
     }
 
     bool pop(size_t queue_id, SvsKeeperStorage::RequestForSession & request)
     {
         assert(queue_id != 0 && queue_id <= queues.size());
-        return queues[queue_id]->pop(request);
+//        return queues[queue_id]->pop(request);
+        return queues[queue_id]->try_dequeue<SvsKeeperStorage::RequestForSession>(request);
     }
 
     bool tryPop(size_t queue_id, SvsKeeperStorage::RequestForSession & request, UInt64 wait_ms = 0)
     {
         assert(queue_id < queues.size());
-        return queues[queue_id]->tryPop(request, wait_ms);
+//        return queues[queue_id]->tryPop(request, wait_ms);
+        return queues[queue_id]->wait_dequeue_timed(request, wait_ms * 1000);
     }
 
     bool tryPopMicro(size_t queue_id, SvsKeeperStorage::RequestForSession & request, UInt64 wait_micro = 0)
     {
         assert(queue_id < queues.size());
-        return queues[queue_id]->tryPopMicro(request, wait_micro);
+//        return queues[queue_id]->tryPopMicro(request, wait_micro);
+        return queues[queue_id]->wait_dequeue_timed(request, wait_micro);
     }
 
 
@@ -57,7 +71,7 @@ struct RequestsQueue
     {
         for (const auto & queue : queues)
         {
-            if (queue->tryPop(request, wait_ms))
+            if (queue->wait_dequeue_timed(request, wait_ms * 1000))
                 return true;
         }
         return false;
@@ -65,16 +79,22 @@ struct RequestsQueue
 
     size_t size() const
     {
+//        size_t size{};
+//        for (const auto & queue : queues)
+//            size += queue->size();
+//        return size;
         size_t size{};
         for (const auto & queue : queues)
-            size += queue->size();
+            size += queue->size_approx();
         return size;
     }
 
     size_t size(size_t queue_id) const
     {
+//        assert(queue_id < queues.size());
+//        return queues[queue_id]->size();
         assert(queue_id < queues.size());
-        return queues[queue_id]->size();
+        return queues[queue_id]->size_approx();
     }
 
     bool empty() const
