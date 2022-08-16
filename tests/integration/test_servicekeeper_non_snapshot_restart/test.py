231880b979cbef71e443336e1a0284d604b15822
zhangxiao871 <zhangxiao871@jd.com>
2022-08-16 17:11:13 +0800

    test_servicekeeper_non_snapshot_restart test_servicekeeper_upgrade

--- tests/integration/test_servicekeeper_non_snapshot_restart/test.py
+++ tests/integration/test_servicekeeper_non_snapshot_restart/test.py
@@ -67,7 +67,7 @@ def get_fake_zk(cluster1, nodename, timeout=30.0):
     return _fake_zk_instance
 
 def compare_stats(stat1, stat2, path):
-    assert stat1.czxid == stat2.czxid, "path " + path + " cxzids not equal for stats: " + str(stat1.czxid) + " != " + str(stat2.zxid)
+    assert stat1.czxid == stat2.czxid, "path " + path + " cxzids not equal for stats: " + str(stat1.czxid) + " != " + str(stat2.czxid)
     assert stat1.mzxid == stat2.mzxid, "path " + path + " mxzids not equal for stats: " + str(stat1.mzxid) + " != " + str(stat2.mzxid)
     assert stat1.version == stat2.version, "path " + path + " versions not equal for stats: " + str(stat1.version) + " != " + str(stat2.version)
     assert stat1.cversion == stat2.cversion, "path " + path + " cversions not equal for stats: " + str(stat1.cversion) + " != " + str(stat2.cversion)
