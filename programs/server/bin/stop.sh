48de902d82366b99f8c9bdddd6703e41d8c5b93d
JackyWoo <wuchienchao@qq.com>
2023-02-25 15:48:35 +0800

    fix build.sh

--- programs/server/bin/stop.sh
+++ programs/server/bin/stop.sh
@@ -1,7 +1,10 @@
 set -eo pipefail
-START_DIR=`dirname "$0"`
-START_DIR=`cd "$START_DIR"; pwd`
-cd $START_DIR/
-pid=`cat raftkeeper.pid`
-kill -15 $pid
-rm raftkeeper.pid
+
+START_DIR=$(dirname "$0")
+START_DIR=$(cd "$START_DIR"; pwd)
+
+cd "$START_DIR"/
+pid=$(cat raftkeeper.pid)
+
+kill -15 "$pid"
+# rm raftkeeper.pid
--- src/Common/ArrayCache.h
+++ src/Common/ArrayCache.h
@@ -725,4 +725,4 @@ public:
     }
 };
 
-template <typename Key, typename Payload> constexpr size_t ArrayCache<Key, Payload>::min_chunk_size;
+//template <typename Key, typename Payload> constexpr size_t ArrayCache<Key, Payload>::min_chunk_size;
