4a3ca6678366662f367241371d212669b3e22ff0
JackyWoo <wuchienchao@qq.com>
2023-03-10 14:36:19 +0800

    Use bridge net in CI runner

---
+++ .github/workflows/run-integration-test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# ${github.workspace}/tests/integration
+work_dir=$1
+# shellcheck disable=SC2164
+cd "$work_dir"
+
+#test_cases=($(ls "$work_dir" | grep test_))
+test_cases=(test_multinode_simple)
+ test_result="succeed"
+
+echo "Total ${#test_cases[*]} test cases to run."
+
+# shellcheck disable=SC1073
+for test_case in ${test_cases[*]}
+do
+    echo -e "\n================= Run test $test_case ================="
+    ./runner --binary "${work_dir}"/../../build/programs/raftkeeper  \
+             --base-configs-dir "${work_dir}"/../../programs/server \
+             --use-net-host "$test_case"
+    # shellcheck disable=SC2181
+    if [ $? -ne 0 ]; then
+         test_result="failed"
+        # print docker log and raftkeeper instances log
+        echo -e "\n================= Test $test_case failed! ================="
+        raftkeeper_instances=$(ls "$test_case"/_instances | grep node)
+        #echo "================= $test_case docker log ================="
+        #sudo cat "$test_case"/_instances/docker.log
+        for raftkeeper_instance in ${raftkeeper_instances[*]}
+        do
+            echo -e "\n================= $test_case raftkeeper $raftkeeper_instance log ================="
+            sudo cat "$test_case"/_instances/"$raftkeeper_instance"/logs/raftkeeper-server.log
+        done
+        break;
+    fi
+done
+
+if [ $test_result == "failed" ]; then
+    exit 1;
+fi
