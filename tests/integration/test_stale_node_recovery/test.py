7f45cc51efed9ca303b315ee0558ff9f775f53e1
JackyWoo <wuchienchao@qq.com>
2023-11-06 18:20:00 +0800

    Fix tests

--- tests/integration/test_stale_node_recovery/test.py
+++ tests/integration/test_stale_node_recovery/test.py
@@ -33,38 +33,36 @@ def test_stale_node_recovery(started_cluster):
         for child_node in range(10):
             node1_zk.create("/test_stale_node_recovery/" + str(child_node))
 
+        # make node1 leader
+        data = node1.send_4lw_cmd(cmd='rqld')
+        assert data == "Sent leadership request to leader."
+        # pull wait to become leader
+        retry = 0
+        while not node1.is_leader() and retry < 30:
+            time.sleep(1)
+            retry += 1
+        if retry == 30:
+            print("node1 does not become leader in 30s, maybe there is something wrong.")
+        assert node1.is_leader()
+
         # stop node3
         node3_zk = node3.get_fake_zk()
         node3.stop_raftkeeper(kill=True)
 
-        # make node1 leader
-        data = node1.send_4lw_cmd(cmd='rqld')
-        assert data == "Sent leadership request to leader."
-
-        if not node1.is_leader():
-            # pull wait to become leader
-            retry = 0
-            while not node1.is_leader() and retry < 30:
-                time.sleep(1)
-                retry += 1
-            if retry == 30:
-                print("node1 does not become leader in 30s, maybe there is something wrong.")
-        assert node1.is_leader()
-
         # delete some node
         node1_zk.delete("/test_stale_node_recovery/0")
 
-        # creating snapshot will compact log,  so node1(leader) has no log for deleting "/test_stale_node_recovery/0"
+        # creating snapshot which will compact log, so node1(leader) has no log for deleting "/test_stale_node_recovery/0"
         last_snapshot_idx = node1.send_4lw_cmd(cmd='csnp')
         data = node1.send_4lw_cmd(cmd='lgif')
+        retry = 0
+        while f"last_snapshot_idx\t{last_snapshot_idx}" not in data and retry < 30:
+            time.sleep(1)
+            retry += 1
+            data = node1.send_4lw_cmd(cmd='lgif')
         if f"last_snapshot_idx\t{last_snapshot_idx}" not in data:
-            retry = 0
-            while f"last_snapshot_idx\t {last_snapshot_idx}" not in data and retry < 30:
-                time.sleep(1)
-                retry += 1
-            if retry == 30:
-                print("node1 does not complete snapshot in 30s, maybe there is something wrong.")
-                assert False
+            print("node1 does not complete snapshot in 30s, maybe there is something wrong.")
+            assert False
 
         # start node3, node3 will load snapshot from node1
         node3.start_raftkeeper()
