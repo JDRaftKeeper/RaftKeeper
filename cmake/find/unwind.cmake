6cd9f6575d13410378f54bb5a315b8daea890a99
JackyWoo <wuchienchao@qq.com>
2024-05-11 17:06:33 +0800

    Refactoring cmake scripts

--- cmake/find/unwind.cmake
+++ cmake/find/unwind.cmake
@@ -1,13 +1,8 @@
-option (USE_UNWIND "Enable libunwind (better stacktraces)" ${ENABLE_LIBRARIES})
+option (USE_UNWIND "Enable libunwind (better stacktraces)" ON)
 
-if (USE_UNWIND)
-    add_subdirectory(contrib/libunwind-cmake)
-    set (UNWIND_LIBRARIES unwind)
-    set (EXCEPTION_HANDLING_LIBRARY ${UNWIND_LIBRARIES})
+add_subdirectory(contrib/libunwind-cmake)
+set (UNWIND_LIBRARIES unwind)
+set (EXCEPTION_HANDLING_LIBRARY ${UNWIND_LIBRARIES})
 
-    message (STATUS "Using libunwind: ${UNWIND_LIBRARIES}")
-else ()
-    set (EXCEPTION_HANDLING_LIBRARY gcc_eh)
-endif ()
-
+message (STATUS "Using libunwind: ${UNWIND_LIBRARIES}")
 message (STATUS "Using exception handler: ${EXCEPTION_HANDLING_LIBRARY}")
--- cmake/target.cmake
+++ cmake/target.cmake
@@ -16,37 +16,15 @@ endif ()
 
 if (CMAKE_CROSSCOMPILING)
     if (OS_DARWIN)
-        # FIXME: broken dependencies
-        set (ENABLE_PROTOBUF OFF CACHE INTERNAL "")
-        set (ENABLE_GRPC OFF CACHE INTERNAL "") # no protobuf -> no grpc
-
-        set (USE_SNAPPY OFF CACHE INTERNAL "")
-        set (ENABLE_PARQUET OFF CACHE INTERNAL "") # no snappy and protobuf -> no parquet
-        set (ENABLE_ORC OFF CACHE INTERNAL "") # no arrow (parquet) -> no orc
-
-        set (ENABLE_ICU OFF CACHE INTERNAL "")
-        set (ENABLE_FASTOPS OFF CACHE INTERNAL "")
+        # Handle macOS specific configuration
     elseif (OS_LINUX OR OS_ANDROID)
         if (ARCH_AARCH64)
-            # FIXME: broken dependencies
-            set (ENABLE_PROTOBUF OFF CACHE INTERNAL "")
-            set (ENABLE_GRPC OFF CACHE INTERNAL "")
-
-            set (ENABLE_PARQUET OFF CACHE INTERNAL "")
-            set (ENABLE_ORC OFF CACHE INTERNAL "")
-
-            set (ENABLE_MYSQL OFF CACHE INTERNAL "")
+            # Handle Linux and Android specific configuration for AArch64
         endif ()
     elseif (OS_FREEBSD)
-        # FIXME: broken dependencies
-        set (ENABLE_PROTOBUF OFF CACHE INTERNAL "")
-        set (ENABLE_GRPC OFF CACHE INTERNAL "")
-
-        set (ENABLE_ORC OFF CACHE INTERNAL "")  # no protobuf -> no parquet -> no orc
-
-        set (ENABLE_EMBEDDED_COMPILER OFF CACHE INTERNAL "")
+        # Handle FreeBSD specific configuration
     else ()
-        message (FATAL_ERROR "Trying to cross-compile to unsupported system: ${CMAKE_SYSTEM_NAME}!")
+        message(FATAL_ERROR "Trying to cross-compile to unsupported system: ${CMAKE_SYSTEM_NAME}!")
     endif ()
 
     # Don't know why but CXX_STANDARD doesn't work for cross-compilation
