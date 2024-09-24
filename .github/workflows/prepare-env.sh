7f70847e68880b6cb9565e005e117f3a593578fb
lzydmxy <13126752315@163.com>
2024-09-24 15:47:50 +0800

    Fix llvm download script

--- .github/workflows/prepare-env.sh
+++ .github/workflows/prepare-env.sh
@@ -6,17 +6,17 @@ llvm_priority=300
 sudo apt install -y ninja-build ccache
 
 # Check if the desired LLVM version is already installed
-if ! llvm-config-${llvm_version} --version &>/dev/null; then
-  sudo curl -sL https://apt.llvm.org/llvm.sh | sudo bash -s -- 18 all
+if ! llvm-config-"${llvm_version}" --version &>/dev/null; then
+  sudo curl -sL https://apt.llvm.org/llvm.sh | sudo bash -s -- "${llvm_version}" all
 else
   echo "LLVM ${llvm_version} is already installed."
 fi
 
 # make clang as default compiler
-sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-${llvm_version} ${llvm_priority}
-sudo update-alternatives --install /usr/bin/llvm-symbolizer llvm-symbolizer /usr/bin/llvm-symbolizer-${llvm_version} ${llvm_priority}
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-${llvm_version} ${llvm_priority}
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-${llvm_version} ${llvm_priority}
+sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-"${llvm_version}" ${llvm_priority}
+sudo update-alternatives --install /usr/bin/llvm-symbolizer llvm-symbolizer /usr/bin/llvm-symbolizer-"${llvm_version}" ${llvm_priority}
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-"${llvm_version}" ${llvm_priority}
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-"${llvm_version}" ${llvm_priority}
 
 clang-${llvm_version} -v
 
